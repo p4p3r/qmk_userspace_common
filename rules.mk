6d3e27a01a15c4dc1f3ba08a6d94ef0a440592f4
p4p3r <me@p4p3r.dev>
2025-05-12 13:19:26 +0200

    Refactor main rules.mk

--- rules.mk
+++ rules.mk
@@ -1,22 +1,21 @@
 # Disable unused features
-MAGIC_ENABLE = no
+CAPS_WORD_ENABLE = no
 CONSOLE_ENABLE = no
-UNICODE_ENABLE = no
 GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
 SPACE_CADET_ENABLE = no
 SWAP_HANDS_ENABLE = no
-BOOTMAGIC_ENABLE = no
+UNICODE_ENABLE = no
 TAP_DANCE_ENABLE = no
-RGB_MATRIX_ENABLE = no
-RGBLIGHT_ENABLE = no
-VIA_ENABLE = no
-CAPS_WORD_ENABLE = no
 
 # Enable common features
-LTO_ENABLE = yes
-EXTRAKEY_ENABLE = yes
-MOUSEKEY_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
 COMBO_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+VIA_ENABLE = yes
+WPM_ENABLE=yes
 
 INTROSPECTION_KEYMAP_C = p4p3r.c
 
@@ -24,32 +23,17 @@ INTROSPECTION_KEYMAP_C = p4p3r.c
 ifeq ($(strip $(NANOBOOT)), yes)
 	BOOTLOADER = qmk-hid
 	BOOTLOADER_SIZE = 512
+
+	# Preserve space
+	BOOTMAGIC_ENABLE = no
+	VIA_ENABLE = no
+	WPM_ENABLE=no
 endif
 
-# RGB Matrix boards
-ifeq ($(strip $(KEYBOARD)), $(filter $(KEYBOARD), crkbd/rev1 splitkb/aurora/sweep/rev1 bastardkb/charybdis/3x5))
-    RGB_MATRIX_ENABLE = yes
+# RGB Matrix/Underglow boards
+ifneq ($(filter yes,$(RGB_MATRIX_ENABLE) $(RGBLIGHT_ENABLE)),)
 	SRC += rgb.c
 endif
 
-# RGB Underglow boards
-ifeq ($(strip $(KEYBOARD)), $(filter $(KEYBOARD), crkbd/rev1 splitkb/kyria/rev1))
-    RGBLIGHT_ENABLE = yes
-	SRC += rgb.c
-endif
-
-# Kyria
-ifeq ($(strip $(KEYBOARD)), $(filter $(KEYBOARD), splitkb/kyria/rev1))
-	WPM_ENABLE=yes
-endif
-
-# Charybdis
-ifeq ($(strip $(KEYBOARD)), $(filter $(KEYBOARD), splitkb/aurora/sweep/rev1 bastardkb/charybdis/3x5))
-    BOOTMAGIC_ENABLE = yes
-	VIA_ENABLE = yes
-endif
-
-# BKB-like layout
-ifeq ($(strip $(BKB_LAYOUT)), yes)
-    OPT_DEFS += -DBKB_LAYOUT
-endif
+# TODO: reverse behavior
+OPT_DEFS += -DBKB_LAYOUT
